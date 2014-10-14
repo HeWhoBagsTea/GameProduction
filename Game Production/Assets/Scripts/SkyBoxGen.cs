03a3e27f03c5a4d9cb77771c3843a8e52b244e3d
Xious Ryuzaki <abyssmal_dawn3000@yahoo.com>
2014-10-14 12:00:54 -0600

    Added Assets and script for Skybox randomization. (Hoping I didn't grab any of the Library files, still need to talk to Anthony about that)

---
+++ Game Production/Assets/Scripts/SkyBoxGen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyBoxGen : MonoBehaviour {
+	public Material[] skyboxMats;
+	private int skyNum;
+
+	// Use this for initialization
+	void Start () 
+	{
+		skyNum = Random.Range (0, 3);
+		if (skyNum == 0) 
+		{
+			RenderSettings.skybox = skyboxMats[0];
+		};
+		if (skyNum == 1) 
+		{
+			RenderSettings.skybox = skyboxMats[1];
+		};
+		if (skyNum == 2) 
+		{
+			RenderSettings.skybox = skyboxMats[2];
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
