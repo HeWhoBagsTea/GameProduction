28134d9e78c083b9758eeb5bfbab4dab8d0ca150
Matthew Radcliffe <mradcliffe@student.neumont.edu>
2014-10-14 11:31:27 -0600

    GamePrduction 0.0.1A

---
+++ Game Production/Assets/CodeGameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CodeGameController : MonoBehaviour {
+
+	public GameObject tilePlains;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
+
+			if(Physics.Raycast(ray, out hit)) {
+				GameObject hitObject = hit.collider.gameObject;
+				CodeTilePlains hitTile = hitObject.GetComponent<CodeTilePlains>();
+				if(hitTile) {
+					hitTile.highlightWithin(2);
+					hitTile.selected(1);
+				}
+			}
+		}
+	}
+
+}
---
+++ Game Production/Assets/CodeTilePlains.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CodeTilePlains : MonoBehaviour {
+
+	public Material[] controlRingColors;
+	public Material[] tileHighlight;	
+
+	private int controller = -1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void selected(int highLight) {
+		MeshRenderer planeRenderer = transform.FindChild ("TerrainPlains").GetComponentInChildren<MeshRenderer> ();
+		planeRenderer.material = tileHighlight[highLight];
+	}
+
+	public void highlightWithin(int radius) {
+		radius = radius * 2;
+		Collider[] hitColliders = Physics.OverlapSphere (transform.position, radius);
+		int i = 0;
+
+		while (i < hitColliders.Length) {
+			CodeTilePlains temp = hitColliders[i].GetComponentInParent<CodeTilePlains>();
+			temp.selected(2);
+			i++;
+		}
+	}
+
+	public void deselect() {
+		MeshRenderer planeRenderer = transform.FindChild ("TerrainPlains").GetComponentInChildren<MeshRenderer> ();
+		planeRenderer.material = tileHighlight[0];
+	}
+
+	public void setControl(int player) {
+		MeshRenderer planeRenderer = transform.FindChild ("ControlRing").GetComponentInChildren<MeshRenderer> ();
+		planeRenderer.material = controlRingColors [player];
+		controller = player;
+	}
+
+}
