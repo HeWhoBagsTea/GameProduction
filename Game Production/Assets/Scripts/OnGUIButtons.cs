58c7635494e217e72991c0e139ad91e914fd0005
Xious Ryuzaki <abyssmal_dawn3000@yahoo.com>
2014-10-16 14:45:23 -0600

    Added a button, still completely implementing.

---
+++ Game Production/Assets/Scripts/OnGUIButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnGUIButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (20, 40, 80, 20), "End Turn")) 
+		{
+			CodeGameController.playersTurn = 2;
+			Debug.Log ("You have ended your turn.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
