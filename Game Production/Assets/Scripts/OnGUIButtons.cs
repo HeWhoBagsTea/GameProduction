d2db9e4b94245903f307e43b082d4327af94d46d
Xious Ryuzaki <abyssmal_dawn3000@yahoo.com>
2014-10-17 14:26:10 -0600

    Button position / Still working on smooth orbit. But am able to move on to something else as well.

--- Game Production/Assets/Scripts/OnGUIButtons.cs
+++ Game Production/Assets/Scripts/OnGUIButtons.cs
@@ -4,43 +4,63 @@ using System.Collections;
 public class OnGUIButtons : MonoBehaviour
 {
 
-		//private float rotateSpeed = 5;
-		private Vector3 PoV1 = new Vector3 (30, 270, 0);
-		private Vector3 PoV2 = new Vector3 (30, 90, 0);
-		//private Vector3 targetPosition = Vector3(25, -90, 0);
+	//private float rotateSpeed = 2.5f;
+	private Vector3 PoV1 = new Vector3 (30, 270, 0);
+	private Vector3 PoV2 = new Vector3 (30, 90, 0);
+	private Vector3 player1CamPos = new Vector3 (20, 15, 0.05f);
+	private Vector3 player2CamPos = new Vector3 (-21, 15, -0.5f);
+	private Quaternion newRot;
+	//private Vector3 targetPosition = Vector3(25, -90, 0);
 		
-		void Start ()
-		{
+	void Start ()
+	{
 			decidePlayerPOV ();
-		}
+	}
 		
-		void decidePlayerPOV ()
-		{
+	void decidePlayerPOV ()
+	{
 			transform.eulerAngles = (CodeGameController.playersTurn == 1) ? PoV1 : PoV2;
-			transform.position = (CodeGameController.playersTurn == 1) ? new Vector3 (20, 15, 0.5f) : new Vector3 (-20, 15, -0.5f);
-		}
+			transform.position = (CodeGameController.playersTurn == 1) ? player1CamPos : player2CamPos;
+	}
+
+//	void smoothCamSwitch()
+//	{
+//		if (CodeGameController.playersTurn == 1) 
+//		{
+//			transform.position = Vector3.Lerp (transform.position, player2CamPos, rotateSpeed * Time.deltaTime);
+//			newRot = Quaternion.Euler (PoV2);
+//			transform.rotation = Quaternion.Slerp (transform.rotation, newRot, rotateSpeed * Time.deltaTime);
+//		}
+//		else 
+//		{
+//			transform.position = Vector3.Lerp (transform.position, player1CamPos, rotateSpeed * Time.deltaTime);
+//			newRot = Quaternion.Euler (PoV1);
+//			transform.rotation = Quaternion.Slerp (transform.rotation, newRot, rotateSpeed * Time.deltaTime);
+//		}
+//	}
 		
+	void OnGUI ()
+	{
+			GameObject[] units = GameObject.FindGameObjectsWithTag ("Unit");
 		
-		void OnGUI ()
-		{
-				GameObject[] units = GameObject.FindGameObjectsWithTag ("Unit");
-			
-				if (GUI.Button (new Rect (20, 40, 80, 20), "End Turn")) 
-				{
+			if (GUI.Button (new Rect (Screen.width * 0.01f, Screen.height * 0.9f, Screen.width * 0.1f, Screen.height * 0.06f), "End Turn")) 
+			{
 					foreach (GameObject u in units) 
 					{
 						u.GetComponent<UnitBase> ().resolveTurn ();
 					}
 					CodeGameController.playersTurn = (CodeGameController.playersTurn == 1) ? 2 : 1;    
 					decidePlayerPOV ();
-				
+					//smoothCamSwitch ();
+					
+					
+
 					Debug.Log ("You have ended your turn.");
-				}
-		}
+			}
+	}
 
-		void update ()
-		{
-
-		}
-
+	void update ()
+	{
+	
+	}
 }
