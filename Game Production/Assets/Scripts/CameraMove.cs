53d75e168806af1d04b1b264dd876a2bc16d4bdf
Xious Ryuzaki <abyssmal_dawn3000@yahoo.com>
2014-10-20 11:22:25 -0600

    Camera should function well for first iteration.

--- Game Production/Assets/Scripts/CameraMove.cs
+++ Game Production/Assets/Scripts/CameraMove.cs
@@ -17,26 +17,28 @@ public class CameraMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = new Vector3(0, 0, 0);
-		if((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - mDelta) && this.transform.position.z < maxZ)
-		{
-			pos = new Vector3(0, 0, (1 * Time.deltaTime * speed));
-			this.transform.position += pos;
-		}
-		if((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || Input.mousePosition.y < mDelta) && this.transform.position.z > minZ)
-		{
-			pos = new Vector3(0, 0, (1 * Time.deltaTime * speed));
-			this.transform.position -= pos;
-		}
-		if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || Input.mousePosition.x < mDelta) && this.transform.position.x > minX)
+		Vector3 pos = new Vector3 (0, 0, 0);
+		
+		if((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || Input.mousePosition.y <= Screen.height - mDelta) && this.transform.position.x < maxX)
 		{
 			pos = new Vector3((1 * Time.deltaTime * speed), 0, 0);
-			this.transform.position -= pos;
+			this.transform.position += (CodeGameController.playersTurn == 1) ? pos : -pos;
+
 		}
-		if((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - mDelta) && this.transform.position.x < maxX)
+		if((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || Input.mousePosition.y > mDelta) && this.transform.position.x > minX)
 		{
-			pos = new Vector3((1 * Time.deltaTime * speed), 0, 0);
-			this.transform.position += pos;
+			pos = new Vector3((1 * Time.deltaTime * speed),0,0);
+			this.transform.position -= (CodeGameController.playersTurn == 1) ? pos : -pos;
+		}
+		if((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || Input.mousePosition.x < mDelta) && this.transform.position.z > minZ)
+		{
+			pos = new Vector3(0,0,(1 * Time.deltaTime * speed));
+			this.transform.position -= (CodeGameController.playersTurn == 1) ? pos : -pos;
+		}
+		if((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - mDelta) && this.transform.position.z < maxZ)
+		{
+			pos = new Vector3(0,0,(1 * Time.deltaTime * speed));
+			this.transform.position += (CodeGameController.playersTurn == 1) ? pos : -pos;
 		}
 	}
 }
