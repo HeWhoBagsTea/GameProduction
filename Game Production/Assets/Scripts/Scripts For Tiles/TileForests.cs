96201b849e47e6ec80b2e90432852a07aace0415
Chris <Cbaker@student.neumont.edu>
2014-10-27 10:58:16 -0600

    Added updated models, deleted most simple models

---
+++ Game Production/Assets/Scripts/Scripts For Tiles/TileForests.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileForests : TileStandard {
+
+	override public void init()
+	{
+		this.originalMoveCost = 2;
+	}
+	//Siege units should be able to move through forests but not attack while in them.
+
+
+}
---
+++ Game Production/Assets/Scripts/Scripts For Tiles/TileMountains.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMountains : TileStandard {
+
+	override public void init()
+	{
+		this.originalMoveCost = 3;
+	}
+	//Should units be able to shoot through mountain tiles?
+
+}
