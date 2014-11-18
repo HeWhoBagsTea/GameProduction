d13aa45bdc60bbbf587544c6dd9b773127e1ed5d
Xious Ryuzaki <abyssmal_dawn3000@yahoo.com>
2014-11-18 11:03:48 -0700

    Still trying to get pop-ups to go away for next players turn. (Terrain only one broken)

---
+++ Game Production/Assets/Scripts/Popup-tutorial/TerrainPopUp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainPopUp : MonoBehaviour {
+
+	private bool hasEntered = false;
+	private bool stayActive = false;
+	
+	void OnMouseEnter() {
+		hasEntered = true;
+	}
+	
+	void OnMouseExit() {
+		hasEntered = false;
+	}		
+	
+	void OnGUI() {
+		if (hasEntered) {
+			if(NewGameController.currentPlayer.isTurn && !NewGameController.currentPlayer.isLookingAtPopup && NewGameController.currentPlayer.specialTerrain){ //&& NewGameController.selectedUnit != null && NewGameController.selectedUnit.controller == NewGameController.currentPlayer) {
+				stayActive = true;
+				NewGameController.currentPlayer.isLookingAtPopup = true;
+			}
+		}
+
+		if (stayActive && NewGameController.currentPlayer.specialTerrain && NewGameController.selectedUnit == null && NewGameController.selectedUnit.controller == NewGameController.currentPlayer) {
+			GUI.Box(new Rect(10, 10, 500, 300), "Green to move red 3");
+			if(GUI.Button(new Rect(20, 400, 100, 20), "Close"))
+			{
+				NewGameController.currentPlayer.specialTerrain = false;
+				NewGameController.currentPlayer.isLookingAtPopup = false;
+			}
+		}
+	}
+}
---
+++ Game Production/Assets/Scripts/Popup-tutorial/UnitPopUp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitPopUp : MonoBehaviour 
+{
+	private bool hasEntered = false;
+	private bool stayActive = false;
+
+	void OnMouseEnter() {
+		hasEntered = true;
+	}
+		
+	void OnMouseExit() {
+		hasEntered = false;
+	}		
+
+	void OnGUI() {
+		if (hasEntered) {
+			if(NewGameController.currentPlayer.isTurn && !NewGameController.currentPlayer.isLookingAtPopup && NewGameController.currentPlayer.fistTimeSelectUnit) {
+				stayActive = true;
+				NewGameController.currentPlayer.isLookingAtPopup = true;
+			}
+		}
+
+		if (stayActive && NewGameController.currentPlayer.fistTimeSelectUnit && NewGameController.currentPlayer == this.gameObject.GetComponent<UnitBase>().controller) {
+			GUI.Box(new Rect(10, 10, 500, 300), "Green to move red 2");
+			if(GUI.Button(new Rect(20, 400, 100, 20), "Close"))
+			{
+				NewGameController.currentPlayer.fistTimeSelectUnit = false;
+				NewGameController.currentPlayer.isLookingAtPopup = false;
+			}
+		}
+	}
+}
